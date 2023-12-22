3caceccfe5079b36d27be8a112f502d0b9daeea5
Evgenii Dudnik <johnydvgu@gmail.com>
2023-12-22 15:36:59 +0300

    init webpack config

---
+++ webpack.config.ts
@@ -0,0 +1,72 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import { VueLoaderPlugin } from "vue-loader";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+type Mode = 'production' | 'development';
+
+interface EnvVariable {
+    mode: Mode;
+    port: number;
+}
+
+export default (env: EnvVariable) => {
+    const isDev = env.mode === 'development';
+
+    const config = {
+        mode: env.mode ?? 'development',
+        entry: path.resolve(__dirname, 'src', 'main.ts'),
+        module: {
+            rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                options: {
+                    appendTsSuffixTo: [/\.vue$/]
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }],
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js'],
+            alias: {
+                'vue':'vue/dist/vue.esm-bundler.js'
+            }
+        },
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: '[name].[contenthash].js',
+            clean: true,
+        },
+        devtool: isDev && 'inline-source-map',
+        devServer: isDev ? {
+            port: env.port ?? 3000,
+            open: true
+        } : undefined,
+        plugins: [
+            new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
+            isDev && new webpack.ProgressPlugin(),
+            !isDev && new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash].css',
+                chunkFilename: 'css/[name].[contenthast:8].css',
+            }),
+            new VueLoaderPlugin(),
+        ].filter(Boolean),
+    }
+
+    return config;
+}
+
